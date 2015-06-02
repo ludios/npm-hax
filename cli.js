251dc525d3eeb00d9b38483fbb99a5c60ee2a32d
Ivan Kozik <ivan@ludios.org>
2015-06-02 02:17:09 +0000

    Read blacklist from process.env

--- cli.js
+++ cli.js
@@ -9,6 +9,10 @@ const Module = require('module');
 const _require = Module.prototype.require;
 const _readJson = require('read-package-json');
 
+function isObject(obj) {
+	return obj && typeof obj === "object";
+}
+
 function readJson(file, log_, strict_, cb_) {
 	let log, strict, cb;
 	for(let i = 1; i < arguments.length - 1; i++) {
@@ -25,17 +29,32 @@ function readJson(file, log_, strict_, cb_) {
 	cb = arguments[arguments.length - 1];
 
 	// We replace cb with our own callback that modifies
-	// data.dependencies before it is given to the user.
+	// data.{dependencies, devDependencies} first
 	_readJson(file, log, strict, function(err, data) {
-		if(data) {
-			if(typeof data.dependencies === "object") {
-				delete data.dependencies["request"];
+		const depsBlacklist = process.env.DEPS_BLACKLIST;
+		if(isObject(data) && depsBlacklist) {
+			for(const entry of depsBlacklist.split(' ')) {
+				if(!entry) {
+					continue;
+				}
+				_ = entry.split('/');
+				const depender = _[0];
+				const dependee = _[1];
+				if(depender === data.name) {
+					if(isObject(data.dependencies)) {
+						delete data.dependencies[dependee];
+					}
+					if(isObject(data.devDependencies)) {
+						delete data.devDependencies[dependee];
+					}
+				}
 			}
 		}
 		cb(err, data);
 	});
 }
 
+// Patch require() to load our read-package-json replacement
 Module.prototype.require = function cachePathsRequire(name) {
 	if(name === 'read-package-json') {
 		return readJson;
